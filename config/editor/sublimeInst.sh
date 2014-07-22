045f59f3790a7b4c6a068e610dc2a6ded41c2540
stanleymiracle <logicomiracle@gmail.com>
2014-07-21 22:38:49 -0700

    Small changes of package installing

--- config/editor/sublimeInst.sh
+++ config/editor/sublimeInst.sh
@@ -1,6 +1,5 @@
 # Install Sublime Text 2 after downloading
 
-wget http://c758482.r82.cf2.rackcdn.com/Sublime\ Text\ 2.0.1\ x64.tar.bz2
 tar xvjf Sublime\ Text\ 2*.tar.bz2
 mv Sublime\ Text\ 2/ /opt/sublime-text
 ln -s /opt/sublime-text /usr/local/sublime-text
